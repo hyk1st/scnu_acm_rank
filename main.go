0061c7c429350cc1d19a21339b2175ccbff1df6d
黄一肯 <12517711+hyk1st@user.noreply.gitee.com>
2024-02-24 13:37:45 +0800

    正常迭代

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	h := server.Default()
 	h.Use(cors.New(cors.Config{
 		//准许跨域请求网站,多个使用,分开,限制使用*
-		AllowOrigins: []string{"http://10.252.146.164"},
+		AllowOrigins: []string{"*"},
 		//准许使用的请求方式
 		AllowMethods: []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
 		//准许使用的请求表头
