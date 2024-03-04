023d59328fa7ab0f7ab4106e589573cbd7406799
黄一肯 <12517711+hyk1st@user.noreply.gitee.com>
2024-03-04 15:10:43 +0800

    正常迭代

--- biz/handler/personTeamDetail.go
+++ biz/handler/personTeamDetail.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
 	"net/http"
 	"scnu_acm_rank/biz/middle"
 	"scnu_acm_rank/biz/model"
@@ -21,7 +22,19 @@ func TeamDetail(ctx context.Context, c *app.RequestContext) {
 	} else {
 		sql = `SELECT user.name userName, team.name from user, team where team.id = ? AND user.group_id = team.id;`
 	}
-	res := make(map[string]interface{})
-	model.DB.Raw(sql, id).Find(res)
-	c.JSON(http.StatusOK, middle.SuccessResp("", res))
+
+	res := make([]map[string]interface{}, 0)
+	model.DB.Raw(sql, id).Find(&res)
+	if len(res) == 0 {
+		c.JSON(http.StatusOK, middle.SuccessResp("", nil))
+		return
+	}
+	list := make([]string, 0, 3)
+	for _, v := range res {
+		list = append(list, v["userName"].(string))
+	}
+	c.JSON(http.StatusOK, middle.SuccessResp("", utils.H{
+		"name":  res[0]["name"],
+		"users": list,
+	}))
 }
